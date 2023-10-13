62bed3a84e1a30fe3343611b639c0f6ce7a0d720
Анатолий Корзунов <144378690+Mayzer7@users.noreply.github.com>
2023-10-13 22:50:57 +0300

    Update main.cpp

--- Project1/main.cpp
+++ Project1/main.cpp
@@ -4,7 +4,7 @@
 using namespace std;
 int main()
 {
-	cout << "1." << setw(100) << "Ivanov" << endl;
+	cout << "1." << setw(10) << "Ivanov" << endl;
 	cout << "2." << setw(10) << left  << "Ivanov" << endl;
 	cout << "3." << setw(10) << right << "Ivanov" << endl;
 	return 0;
